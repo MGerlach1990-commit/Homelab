69e3f5041b49f9ade032ed361b8ab8b8c2196354
MGerlach <MGerlach@users.noreply.github.com>
2026-09-02 11:49:31 +0200

    Fix project directory path in start-dev-server.bat

--- .astro-site/start-dev-server.bat
+++ .astro-site/start-dev-server.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Start Astro Dev Server on system startup
-cd /d "C:\Users\MGerlach\Documents\MartinHomelab\.astro-site"
+cd /d "C:\Users\MGerlach\Documents\Homelab\.astro-site"
 npm run dev
 pause
